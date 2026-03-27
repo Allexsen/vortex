da71caa079c01bddf77ab00853614aafd6224578
Aleksi <asvanidze12@gmail.com>
2026-03-27 11:13:25 +0400

    refactor: proper redis struct
    moving from the exposed redis.Client to encapsulated one, slowly changing the signature.

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,8 +1,23 @@
 package cache
 
 import (
+	"context"
+	"time"
+
 	"github.com/redis/go-redis/v9"
 )
+
+type RedisCache struct {
+	client *redis.Client
+	prefix string
+}
+
+func NewRedisCache(client *redis.Client, prefix string) *RedisCache {
+	return &RedisCache{
+		client: client,
+		prefix: prefix,
+	}
+}
 
 func NewRedisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
@@ -11,4 +26,16 @@ func NewRedisClient() *redis.Client {
 		DB:       0,                // use default DB
 		PoolSize: 10,               // set pool size
 	})
+}
+
+func (c *RedisCache) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return c.client.Set(ctx, c.prefix+key, value, ttl).Err()
+}
+
+func (c *RedisCache) Get(ctx context.Context, key string) ([]byte, error) {
+	data, err := c.client.Get(ctx, c.prefix+key).Bytes()
+	if err == redis.Nil {
+		return nil, nil // Cache miss
+	}
+	return data, err
 }
